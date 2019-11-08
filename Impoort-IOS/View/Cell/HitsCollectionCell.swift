eb6e75c54efd7b291b30d618eaf0366d48a2be67
yusufalicezik <yusufalicezik35@gmail.com>
2019-11-08 23:57:22 +0300

    FlickrLayout added

---
+++ Impoort-IOS/View/Cell/HitsCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  HitsCollectionCell.swift
+//  Impoort-IOS
+//
+//  Created by Yusuf ali cezik on 8.11.2019.
+//  Copyright © 2019 Yusuf Ali Cezik. All rights reserved.
+//
+
+import UIKit
+
+class HitsCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var postImageView: UIImageView!
+    
+}
